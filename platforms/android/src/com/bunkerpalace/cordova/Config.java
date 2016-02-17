c4d79552aa4eab3a2e8579a22df14b97a6018045
arctouch-brunowolff <bruno.wolff@arctouch.com>
2016-02-17 15:20:01 -0200

    Update the plugin to work on newer Android versions
    For newer android versions (5+), it will use the YouTube API player.

---
+++ platforms/android/src/com/bunkerpalace/cordova/Config.java
@@ -0,0 +1,9 @@
+package com.bunkerpalace.cordova;
+
+/**
+ * Created by brunowolff on 2/16/16.
+ */
+public class Config {
+
+    public static final String YOUTUBE_API_KEY = "YOUTUBE_DATA_API_KEY";
+}
